37cff4036dfc80ec2560bb51e322eb2e87d65390
JamzumSum <zzzzss990315@gmail.com>
2019-02-21 13:34:45 +0800

    船新版本！

---
+++ frmain.cpp
@@ -0,0 +1,135 @@
+/*
+* main.cpp
+*
+*  Created on: 2017ƒÍ12‘¬13»’
+*      Author: JamzumSum
+*/
+#pragma once
+#include "GUIlib\GUI.hpp"
+#include "resource.h"
+#include <ShellAPI.h>
+
+using namespace std;
+
+extern string httpGet(string);
+extern string regurl(string, string&);
+extern bool download(string, string);
+static void setWallpaper(string);
+
+Label* notify = NULL;
+ProgressBar* progress = NULL;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow){
+	form frmain(NULL, "JamzumSum", "Bing Picker", 0, 0, 640, 360);
+	form about(&frmain, "JamzumSum", "About");
+	frmain.Event_On_Create = [&about, &frmain]() {
+		frmain.enableRBmenu();
+		frmain.createMenu(frmain.RBmenu, "About", [&]() {
+			Label I(&about, 24, 24, 344, 216,
+				"Developer:\r\n"
+				"	JamzumSum\n\n"
+				"Credit:\n"
+				//"	@opencv - OpenCV Project\n"
+				"	@sorcerer - Bing icon\n"
+				"\nThanks for using.\n");
+			about.Event_Load_Complete = [&]() {
+				about.resize(392, 280);
+				about.move(336, 112);
+				I.setFont("Œ¢»Ì—≈∫⁄", 20);
+			};
+			about.create();
+			about.run();
+			about.msgLoop.detach();
+		});
+	};
+	frmain.setIcon(MAKEINTRESOURCE(IDI_ICON1), MAKEINTRESOURCE(IDI_ICON2));
+
+	Label info(&frmain, 8, -60, 500, 36, "Tap `Download`");
+	ProgressBar dld(&frmain, 8, -80, -32, 16, "downloading...");
+	Button setWall(&frmain, 280, 120, 80, 45, "Download");
+
+	string filename;
+	frmain.feature ^= WS_MAXIMIZEBOX;
+	bool downloaded = false;
+	notify = &info;
+	progress = &dld;
+
+	auto showPic = [&frmain, &downloaded](string pic) {
+		if (!downloaded) return;
+		ShellExecute(frmain.hWnd(), "open", pic.c_str(), NULL, NULL, SW_SHOW);
+	};
+
+	info.Event_On_Click = [&showPic, &filename]() {
+		showPic(filename + ".jpg");
+	};
+
+	frmain.Event_Load_Complete = [&]() {
+		info.setFont("Œ¢»Ì—≈∫⁄", 18);
+		setWall.setFont("Œ¢»Ì—≈∫⁄", 18);
+	};
+	setWall.Event_On_Click = [&]() {
+		if (downloaded) setWallpaper(filename);
+		else {
+			string httpD = httpGet("https://cn.bing.com");
+			string pic;
+			try {
+				pic = regurl(httpD, filename);
+				download(pic, filename);
+			}
+			catch (string msg) {
+				info.name = msg.c_str();
+				return;
+			}
+
+			Sleep(500);
+			downloaded = true;
+			dld.full();
+			showPic(filename + ".jpg");
+			setWall.name = "Set as Wallpaper";
+			setWall.resize(160, 0);
+			setWall.move(240, 0);
+			info.name = ("Today's Bing image: " + filename).c_str();
+		}
+	};
+	frmain.run();
+	frmain.msgLoop.join();
+	return 0;
+}
+
+void setWallpaper(string path) {
+	HRESULT hr = S_OK;
+	HKEY hKey = NULL;
+	hr = HRESULT_FROM_WIN32(RegOpenKeyEx(HKEY_CURRENT_USER,
+		"Control Panel\\Desktop", 0, KEY_READ | KEY_WRITE, &hKey));
+	if (SUCCEEDED(hr))
+	{
+		LPCSTR pszWallpaperStyle;
+		LPCSTR pszTileWallpaper;
+		pszWallpaperStyle = "6";
+		pszTileWallpaper = "0";
+		// …Ë÷√ WallpaperStyle ∫Õ TileWallpaper µΩ◊¢≤·±ÌœÓ.  
+		DWORD cbData = lstrlen(pszWallpaperStyle) * sizeof(*pszWallpaperStyle);
+		hr = HRESULT_FROM_WIN32(RegSetValueEx(hKey, "WallpaperStyle", 0, REG_SZ,
+			reinterpret_cast<const BYTE *>(pszWallpaperStyle), cbData));
+		if (SUCCEEDED(hr)) {
+			cbData = lstrlen(pszTileWallpaper) * sizeof(*pszTileWallpaper);
+			hr = HRESULT_FROM_WIN32(RegSetValueEx(hKey, "TileWallpaper", 0, REG_SZ,
+				reinterpret_cast<const BYTE *>(pszTileWallpaper), cbData));
+		}
+		RegCloseKey(hKey);
+	}
+	char current[MAXSIZE];
+	if (GetCurrentDirectory(MAXSIZE, current)) {
+		string fullpath = current;
+		fullpath += "//";
+		fullpath += path;
+		if (SUCCEEDED(hr)) if (!SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, (PVOID)fullpath.c_str(), SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)) {
+			//hr = HRESULT_FROM_WIN32(GetLastError());
+			/*char s[10];
+			_itoa_s(GetLastError(), s, 10);
+			MessageBox(NULL, TEXT("…Ë÷√±⁄÷Ω ß∞‹£°"), s, MB_OK);*/
+			popError();
+		}
+	}
+	
+}
---
+++ net.cpp
@@ -0,0 +1,149 @@
+#pragma once
+#include "GUIlib\GUI.hpp"
+#include <wininet.h>
+#include <urlmon.h>
+#include <time.h>
+#include <string>
+#include <regex>
+
+#define MAXSIZE 1024
+#define overtime 5000
+
+using namespace std;
+
+string httpGet(string);
+bool download(string, string);
+extern Label* notify;
+extern ProgressBar* progress;
+
+class CBindCallback : public IBindStatusCallback {
+public:
+	CBindCallback() {}
+	~CBindCallback() {}
+	//IBindStatusCallback的方法。除了OnProgress外的其他方法都返回E_NOTIMPL
+	STDMETHOD(OnStartBinding)(DWORD dwReserved, IBinding __RPC_FAR *pib)
+	{
+		return E_NOTIMPL;
+	}
+	STDMETHOD(GetPriority)(LONG __RPC_FAR *pnPriority)
+	{
+		return E_NOTIMPL;
+	}
+	STDMETHOD(OnLowResource)(DWORD reserved)
+	{ 
+		return E_NOTIMPL;
+	}
+
+	//OnProgress在这里
+	STDMETHOD(OnProgress)
+		(ULONG ulProgress, ULONG ulProgressMax, ULONG ulStatusCode, LPCWSTR wszStatusText) = 0;
+
+	STDMETHOD(OnStopBinding)(HRESULT hresult, LPCWSTR szError)
+	{
+		return E_NOTIMPL;
+	}
+	STDMETHOD(GetBindInfo)(DWORD __RPC_FAR *grfBINDF, BINDINFO __RPC_FAR *pbindinfo)
+	{
+		return E_NOTIMPL;
+	}
+	STDMETHOD(OnDataAvailable)(DWORD grfBSCF, DWORD dwSize, FORMATETC __RPC_FAR *pformatetc, STGMEDIUM __RPC_FAR *pstgmed)
+	{
+		return E_NOTIMPL;
+	}
+	STDMETHOD(OnObjectAvailable)(REFIID riid, IUnknown __RPC_FAR *punk)
+	{
+		return E_NOTIMPL;
+	}
+	STDMETHOD_(ULONG, AddRef)()
+	{
+		return 0;
+	}
+	STDMETHOD_(ULONG, Release)()
+	{
+		return 0;
+	}
+	STDMETHOD(QueryInterface)(REFIID riid, void __RPC_FAR *__RPC_FAR *ppvObject)
+	{
+		return E_NOTIMPL;
+	}
+};
+class myCBC : public CBindCallback {
+private:
+	clock_t start, end;
+public:
+	STDMETHOD(OnProgress)
+		(ULONG ulProgress, ULONG ulProgressMax, ULONG ulStatusCode, LPCWSTR wszStatusText) {
+		end = clock();
+		if (end - start > overtime) return INET_E_DOWNLOAD_FAILURE;
+		if (ulProgressMax == 0) notify->name = "unknown file size.";
+		else {
+			progress->setRange(ulProgressMax);
+			progress->setPos(ulProgress);
+		}
+		return S_OK;
+	}
+
+	void begin() {
+		start = clock();
+	}
+};
+
+string httpGet(string url) {
+	string r = "";
+	HINTERNET hSession = InternetOpen("UrlTest", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+	if (hSession)
+	{
+		HINTERNET hHttp = InternetOpenUrlA(hSession, url.c_str(), NULL, 0, INTERNET_FLAG_DONT_CACHE, 0);
+		if (hHttp)
+		{
+			char Temp[MAXSIZE];
+			ULONG Number = 1;
+			while (Number > 0)
+			{
+				InternetReadFile(hHttp, Temp, MAXSIZE - 1, &Number);
+				Temp[Number] = '\0';
+				r += Temp;
+			}
+			InternetCloseHandle(hHttp);
+			hHttp = NULL;
+		}
+		InternetCloseHandle(hSession);
+		hSession = NULL;
+	}
+	return r;
+}
+
+char* replace(char* a) {
+	for (unsigned int i = 0; i < strlen(a); i++) if (a[i] == '/') a[i] = '-';
+	return a;
+}
+
+bool download(string url, string name) throw(string){
+	myCBC cbc;
+	
+	name += ".jpg";
+	DeleteUrlCacheEntry(url.c_str());
+	notify->name = "Download started.";
+	switch (URLDownloadToFile(NULL, url.c_str(), name.c_str(), 0, &cbc)) {
+	case S_OK:
+		cbc.begin();
+		return true;
+	case E_OUTOFMEMORY:
+		throw string("The buffer length is invalid, or there is insufficient memory to complete the operation.");
+	case INET_E_DOWNLOAD_FAILURE:
+		throw string("The specified resource or callback interface was invalid.");
+	default:
+		throw string("Ooops...it seems that some error has occured. Error code: ");
+	}
+	return false;
+}
+
+string regurl(string httpD, string& name) throw(string){
+	smatch rurl, rname;
+	if (!std::regex_search(httpD, rurl, regex("/az.*?_1920x1080.jpg"))) throw string("Sorry. But no picture is crawled. Ask for your dev.");
+	name = rurl.str();
+	std::regex_search(name, rname, regex("rb/.*?_"));
+	name = rname.str().substr(3);
+	name.pop_back();
+	return "http://cn.bing.com" + rurl.str();
+}
